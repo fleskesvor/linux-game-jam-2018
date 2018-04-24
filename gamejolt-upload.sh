1b09dd44ad7f64ab0c7aa732dcf9b42f0d34fb61
Eivind Nilsbakken <eivindkn@gmail.com>
2018-04-24 22:44:52 +0200

    version

--- gamejolt-upload.sh
+++ gamejolt-upload.sh
@@ -3,5 +3,5 @@
 #./butler push "${GAME_NAME}-html5.zip" "${ITCH_IO_USER}/${GAME_NAME}:html5"
 #./butler push "${GAME_NAME}-linux.zip" "${ITCH_IO_USER}/${GAME_NAME}:linux"
 echo "./gjpush -t $GAMEJOLT_API_TOKEN -g $GAMEJOLT_GAME_ID -p $GAMEJOLT_PACKAGE -r 0.1.2 ${GAME_NAME}-mac.zip"
-./gjpush -t $GAMEJOLT_API_TOKEN -g $GAMEJOLT_GAME_ID -p $GAMEJOLT_PACKAGE -r 0.1.2 "${GAME_NAME}-mac.zip"
+./gjpush -t $GAMEJOLT_API_TOKEN -g $GAMEJOLT_GAME_ID -p $GAMEJOLT_PACKAGE -r 0.1.2 "${GAME_NAME}-mac.zip" -v0.42.0
 #./butler push "${GAME_NAME}-windows.zip" "${ITCH_IO_USER}/${GAME_NAME}:windows"
