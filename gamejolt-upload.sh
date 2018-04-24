a78d3484558810237f5d720a3a723675ee686ba5
Eivind Nilsbakken <eivindkn@gmail.com>
2018-04-24 22:56:28 +0200

    release version as ad-hoc platform tag

--- gamejolt-upload.sh
+++ gamejolt-upload.sh
@@ -2,5 +2,5 @@
 
 #./butler push "${GAME_NAME}-html5.zip" "${ITCH_IO_USER}/${GAME_NAME}:html5"
 #./butler push "${GAME_NAME}-linux.zip" "${ITCH_IO_USER}/${GAME_NAME}:linux"
-./gjpush -t $GAMEJOLT_API_TOKEN -g $GAMEJOLT_GAME_ID -p $GAMEJOLT_PACKAGE -r 0.1.2 "${GAME_NAME}-mac.zip" -v 0.42.0
+./gjpush -t $GAMEJOLT_API_TOKEN -g $GAMEJOLT_GAME_ID -p $GAMEJOLT_PACKAGE -r mac -v 0.42.0 "${GAME_NAME}-mac.zip"
 #./butler push "${GAME_NAME}-windows.zip" "${ITCH_IO_USER}/${GAME_NAME}:windows"
